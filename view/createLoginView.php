1696034095bf2bb6f2dc55c0b2771147be02ca42
PascalTed <monemail@email.com>
2018-09-26 17:00:35 +0200

    ajouté formulaire d'inscription dans view/createLoginView.php

---
+++ view/createLoginView.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    <body>
+        <section id="formulaireInscription">
+            <div>
+                <h1>Inscription</h1>
+                <form action="" method="post">
+                    <p>
+                        <label for="pseudo">Pseudo : </label><br />
+                        <input type="text" id="pseudo" name="pseudo" />
+                    </p>
+                    <p>
+                        <label for="email">Adresse Email : </label><br />
+                        <input type="email" id="email" name="email" />
+                    </p>
+                    <p>
+                        <label for="password">Mot de passe : </label><br />
+                        <input type="password" id="password" name="password" />
+                    </p>
+                    <p>
+                        <label for="verifPassword">Vérification du mot de passe : </label><br />
+                        <input type="verifPassword" id="verifPassword" name="verifPassword" />
+                    </p>                
+                </form>
+            </div>
+        </section>
+    </body>
+</html>
+
