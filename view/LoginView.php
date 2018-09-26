d295cfff83fac5e4c46700de65e5406247b2e1a3
PascalTed <monemail@email.com>
2018-09-26 17:16:09 +0200

    ajouté formulaire de connexion dans view/LoginView.php

---
+++ view/LoginView.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    <body>
+        <section id="connexion">
+            <div>
+                <h1>Connexion</h1>
+                <form action="" method="post">
+                    <p>
+                        <label for="pseudo">Pseudo : </label><br />
+                        <input type="text" id="pseudo" name="pseudo" />
+                    </p>
+                    <p>
+                        <label for="password">Mot de passe : </label><br />
+                        <input type="password" id="password" name="password" />
+                    </p>
+                    <p>
+                        <input type="submit" value="Se connecter" />
+                    </p>
+                </form>
+            </div>
+        </section>
+    </body>
+</html>
