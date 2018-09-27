b196590badca69f9e2ea9102cdde89153c3ade1f
PascalTed <monemail@email.com>
2018-09-27 17:16:07 +0200

    Modifié function getComments() dans model/CommentManager.php

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -7,7 +7,7 @@ class CommentManager extends Manager
     public function getComments($postId)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, user_id, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments = $db->prepare('SELECT comments.id, comments.user_id, comments.comment, DATE_FORMAT(comments.comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr, users.pseudo FROM comments INNER JOIN users ON comments.post_id = ? AND users.id = user_id ORDER BY comment_date DESC');
         $comments->execute(array($postId));
 
         return $comments;
