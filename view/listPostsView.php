5f95d1790e45b3be980e63a4072b171b79fbe138
PascalTed <monemail@email.com>
2018-09-27 12:28:59 +0200

    ajouté affichage des billets (ob_start) dans view/listPostsView.php

---
+++ view/listPostsView.php
@@ -0,0 +1,31 @@
+<?php ob_start(); ?>
+
+<h2>Billets du blog :</h2>
+
+<section>
+    
+<?php
+while ($data = $posts->fetch())
+{
+?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($data['title']); ?>
+            <em>le <?= $data['creation_date_fr']; ?></em>
+        </h3>
+        
+        <p>
+            <?= nl2br(htmlspecialchars(substr($data['content'], 0, 100))); ?>
+            <em><a href="index.php?action=post&amp;id=<?= $data['id']; ?>"> ... lire la suite</a></em>
+        </p>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
+    
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
