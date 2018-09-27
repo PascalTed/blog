7108f7bdb3d9dbf270e405afb52ced165cc35a76
PascalTed <monemail@email.com>
2018-09-27 11:50:56 +0200

    ajouté html + php dans view/template.php

---
+++ view/template.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+    
+    <head>
+        <meta charset="utf-8" />
+        <title>Blog de Jean Forteroche</title>
+        <link href="" rel="stylesheet" />
+    </head>
+    
+    <body>
+        <header>
+            <div>
+                <?php $titleHeader ?>
+                <?php $navigation ?>
+            </div>
+        </header>
+        <? $content ?>
+        
+    </body>
+    
+</html>
