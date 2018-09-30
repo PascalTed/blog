89ccd67fc494f8cbdc0ae3cd768c70edae084c51
PascalTed <monemail@email.com>
2018-09-30 12:22:28 +0200

    modifié navigation en menu dans view/template.php

--- view/template.php
+++ view/template.php
@@ -12,7 +12,7 @@
         <header>
             <div>
                 <?= $titleHeader ?>
-                <?= $navigation ?>
+                <?= $menu ?>
             </div>
         </header>
         <?= $content ?>
