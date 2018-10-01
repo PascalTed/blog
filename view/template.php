05929ff8f160b9ca108552bfd43f5552e036da13
PascalTed <monemail@email.com>
2018-10-01 17:15:15 +0200

    Ajouté div seConnecter + link css dans view/template.php

--- view/template.php
+++ view/template.php
@@ -5,7 +5,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Blog de Jean Forteroche</title>
-        <link href="" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet" />
     </head>
     
     <body>
@@ -13,6 +13,21 @@
             <div>
                 <?= $titleHeader ?>
                 <?= $menu ?>
+                <div id="seConnecter">
+                    <form action="" method="post">
+                        <p>
+                            <label for="pseudoConnect">Pseudo : </label><br />
+                            <input type="text" id="pseudoConnect" name="pseudoConnect" />
+                        </p>
+                        <p>
+                            <label for="passwordConnect">Mot de passe : </label><br />
+                            <input type="password" id="passwordConnect" name="passwordConnect" />
+                        </p>
+                        <p>
+                            <input type="submit" value="Se connecter" />
+                        </p>
+                    </form>
+                </div>
             </div>
         </header>
         <?= $content ?>
