4a3365b6d7a9c8858bc72f91416aaa46eb815871
PascalTed <monemail@email.com>
2018-10-01 18:18:13 +0200

    Ajouté event connectWindow dans js/appli.js

---
+++ js/appli.js
@@ -0,0 +1,5 @@
+var connectWindow = document.getElementById("se-connecter");
+connectWindow.addEventListener("click", function() {
+    document.getElementById("seConnecter").style.display = "block";
+    document.body.style.backgroundColor = "grey";
+});
