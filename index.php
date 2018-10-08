2c139820ce9b65b6a8ea7282c78170391d0dcca2
PascalTed <monemail@email.com>
2018-10-08 14:24:01 +0200

    Modifié condition addComment dans index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ try {
         } elseif ($_GET['action'] == 'addComment') {
             if (isset($_GET['idPost']) && $_GET['idPost'] > 0) {
                 if (!empty($_POST['add-comment'])) {
-                    addComment(, $_SESSION['id'], $_GET['idPost'], $_POST['add-comment']);
+                    addComment($_SESSION['id'], $_GET['idPost'], $_POST['add-comment']);
                 }
                 else {
                     throw new Exception('Le champs n\'est pas rempli !');
