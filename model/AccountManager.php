4e9435c427ee729ec2669203f9f8f9e5f84b423b
PascalTed <monemail@email.com>
2018-10-09 13:25:16 +0200

    Supprimé function searchPseudo() + searchEmail() dans model/AccountManager.php

--- model/AccountManager.php
+++ model/AccountManager.php
@@ -4,23 +4,7 @@ require_once("model/Manager.php");
 
 class AccountManager extends Manager
 {
-    public function searchPseudo($pseudo)
-    {
-        $db = $this->dbConnect();
-        $account = $db->prepare('SELECT pseudo FROM users WHERE pseudo = ?');
-        $account->execute(array($pseudo)); 
-        $existPseudo = $account->fetch();
-        return $existPseudo;
-     }
-    
-    public function searchEmail($email)
-    {
-        $db = $this->dbConnect();
-        $account = $db->prepare('SELECT email FROM users WHERE email = ?');
-        $account->execute(array($email)); 
-        $existEmail = $account->fetch();
-        return $existEmail;
-    }
+
     
     public function editAccount()
     {
