a61a0e6fe5ef78a045ff71b4b23d7cb0c21c22ad
PascalTed <monemail@email.com>
2018-10-10 10:51:27 +0200

    Ajouté require, title et ob_start() dans view/adminView.php

---
+++ view/adminView.php
@@ -0,0 +1,17 @@
+<?php require('menuView.php'); ?>
+<?php require('loginView.php'); ?>
+
+<?php $titleHeader = 'Administration du Blog de Jean Forteroche' ?>
+
+
+<?php ob_start(); ?>
+
+
+<div>
+    TEST
+</div>
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?
