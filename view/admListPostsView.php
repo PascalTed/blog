0a4b044f6a571f04b05acba32e6d21fab5d5eac8
PascalTed <monemail@email.com>
2018-10-12 11:33:02 +0200

    Ajouté code initial dans view/admListPostsView.php

---
+++ view/admListPostsView.php
@@ -0,0 +1,45 @@
+<?php require('menuView.php'); ?>
+<?php require('loginView.php'); ?>
+
+<?php $titleHeader = 'Administration du Blog de Jean Forteroche' ?>
+
+
+<?php ob_start(); ?>
+
+<div>
+    <p><a href="index.php?action=admCreatePost">Ajouter des billets</a><a href="index.php?action=admReportComment">Commentaires signalés</a></p>
+</div>
+
+<h2>Billets du blog à modifier ou supprimer:</h2>
+
+
+
+<section>
+    
+<?php
+while ($data = $posts->fetch())
+{
+?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($data['title']); ?>
+            <em>le <?= $data['creation_date_fr']; ?></em>
+        </h3>
+        
+        <p>
+            <?= nl2br(htmlspecialchars(substr($data['content'], 0, 100))); ?>...
+        </p>
+        <p>
+            <a href="index.php?action=admModifPost&amp;idPost=<?= $data['id']; ?>">Modifier ou supprimer</a>
+        </p>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
+    
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
