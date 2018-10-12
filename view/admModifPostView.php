b90b446aed449754ac82ccaac16242ec6eb231b6
PascalTed <monemail@email.com>
2018-10-12 12:35:01 +0200

    Ajouté code intial dans view/admModifPostView.php

---
+++ view/admModifPostView.php
@@ -0,0 +1,47 @@
+<?php require('menuView.php'); ?>
+<?php require('loginView.php'); ?>
+
+<?php $titleHeader = 'Administration du Blog de Jean Forteroche' ?>
+
+
+<?php ob_start(); ?>
+
+<div>
+    <p><a href="index.php?action=admCreatePost">Ajouter des billets</a><a href="index.php">Commentaires signalés</a><a href="index.php?action=admListPosts">Billets créés</a></p>
+</div>
+
+<h2>modifier ou supprimer le billet :</h2>
+
+
+
+<section>
+    
+
+
+<div>
+    <form action="index.php?action=modifyOrRemovePost" method="post">
+        
+        <label for="textarea-titre">Modifier le titre</label>
+        <textarea id="textarea-titre" name="textarea-titre"><?= htmlspecialchars($post['title']); ?></textarea><br />
+
+               
+        <label for="textarea-contenu">Modifier le contenu</label>
+        <textarea id="textarea-contenu" name="textarea-contenu"><?= nl2br(htmlspecialchars(substr($post['content'], 0, 100))); ?></textarea><br />
+        <input type="hidden" name="" value="$data['id']" />
+        
+        <input type="radio" name="setPost" value="modifyPost" id="modifyPost" /> <label for="modifyPost">Modifier le billet</label>
+        
+        <input type="radio" name="setPost" value="removePost" id="removePost" /> <label for="removePost">Supprimer le billet</label><br />
+        
+        <input type="submit" value="Envoyer" />
+
+    </form>
+</div>
+
+
+    
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
