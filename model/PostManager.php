133c67e40bc9cfd2ea15c4e02f8dd43dc3afa67b
PascalTed <monemail@email.com>
2018-10-14 12:15:46 +0200

    Ajouté fonctions modifyPost() et removePost() dans model/PostManager.php

--- model/PostManager.php
+++ model/PostManager.php
@@ -28,6 +28,21 @@ class PostManager extends Manager
         $createPost = $db->prepare('INSERT INTO posts(title, content, creation_date) VALUES(?, ?, NOW())');
         $createPost->execute(array($postTitle, $postContents));
 
-    } 
+    }
+    
+    public function modifyPost ($postId, $textareaTitre, $textareaContenu)
+    {
+        
+        $db = $this->dbConnect();
+        $updatePost = $db->prepare('UPDATE posts SET title = ?, content = ? where id = ?');
+        $updatePost->execute(array($textareaTitre, $textareaContenu, $postId));
+    }
+    
+    public function removePost($postId)
+    {
+        $db = $this->dbConnect();
+        $deletePost = $db->prepare('DELETE FROM posts where id = ?');
+        $deletePost->execute(array($postId));
+    }    
 
 }
