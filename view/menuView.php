b24c09dcf4b64265bab5351512876b8a0f1c5970
PascalTed <monemail@email.com>
2018-10-26 14:04:02 +0200

    Ajouté balises i font awesome (icônes barres et croix) dans view/menuView.php

--- view/menuView.php
+++ view/menuView.php
@@ -1,5 +1,6 @@
 <?php ob_start(); ?>
-
+<i class="fas fa-bars fa-2x" id="open-menu"></i>
+<i class="fas fa-times fa-2x" id="close-menu"></i>
 <ul id="menu">
     <li><a href="index.php"><i class="fas fa-book"></i>Acceuil</a></li>
     <?php
