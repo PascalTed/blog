cde8ab8ea58b298448518c81d1fc5c7adeb4f251
PascalTed <monemail@email.com>
2018-10-28 00:09:28 +0200

    Ajouté balise p + em et modifié balises label dans view/createLoginView.php

--- view/createLoginView.php
+++ view/createLoginView.php
@@ -13,20 +13,21 @@
             <div>
                 
                 <form action="index.php?action=createAccount" method="post">
+                    <p><em>Les champs obligatoires sont indiqués avec *</em></p>
                     <p>
-                        <label for="pseudo">Pseudo : </label><br />
+                        <label for="pseudo">Pseudo*</label><br />
                         <input type="text" id="pseudo" name="pseudo" required /><span id="alertPseudo"></span>
                     </p>
                     <p>
-                        <label for="email">Adresse Email : </label><br />
+                        <label for="email">Adresse Email*</label><br />
                         <input type="email" id="email" name="email" required /><span id="alertEmail"></span>
                     </p>
                     <p>
-                        <label for="password">Mot de passe : </label><br />
+                        <label for="password">Mot de passe*</label><br />
                         <input type="password" id="password" name="password" required /><span id="alertPassword"></span>
                     </p>
                     <p>
-                        <label for="verifPassword">Vérification du mot de passe : </label><br />
+                        <label for="verifPassword">Vérification du mot de passe*</label><br />
                         <input type="Password" id="verifPassword" name="verifPassword" minlength="8" required /><span id="alertVerifPassword"></span>
                     </p>
                     <p>
