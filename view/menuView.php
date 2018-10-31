3ddb2403951109bf3fdac0d1ea2ecc9204c902b9
PascalTed <monemail@email.com>
2018-10-31 01:16:35 +0100

    Nettoyé code dans view/menuView.php

--- view/menuView.php
+++ view/menuView.php
@@ -1,35 +1,40 @@
 <?php ob_start(); ?>
+
+<!-- Font awesome (barres et croix) -->
 <i class="fas fa-bars fa-2x" id="open-menu"></i>
 <i class="fas fa-times fa-2x" id="close-menu"></i>
+
 <ul id="menu">
+    <!-- Font awesome (livre) -->
     <li><a href="index.php"><i class="fas fa-book"></i>Acceuil</a></li>
+    
     <?php
     if (isset($_SESSION['pseudo'])) {
     ?>
         
-    <li><a href="index.php?action=disconnectAccount" id="se-deconnecter">Se déconnecter</a></li>
+        <li><a href="index.php?action=disconnectAccount" id="se-deconnecter">Se déconnecter</a></li>
     
     <?php
     } else {
     ?>
     
-    <li><a href="#" id="se-connecter">Se connecter</a></li>
+        <li><a href="#" id="se-connecter">Se connecter</a></li>
     
     <?php
     }
     ?>
     
-    <li><a href="index.php?action=displayCreateAccount">Créer un compte</a></li>
+        <li><a href="index.php?action=displayCreateAccount">Créer un compte</a></li>
 
     <?php
     if (isset($_SESSION['pseudo']) AND isset($_SESSION['admin']) AND $_SESSION['admin'] == 1) {
     ?>
-    <li><a href="index.php?action=admListPosts">Gérer les billets</a></li>
+        <li><a href="index.php?action=admListPosts">Gérer les billets</a></li>
+    
     <?php
     }
     ?>
     
 </ul>
-
 
 <?php $menu = ob_get_clean(); ?>
