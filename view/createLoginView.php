6ecd481d5aa061e086a724265b87c503afe4ef26
PascalTed <monemail@email.com>
2018-10-31 01:29:07 +0100

    Nettoyé code dans view/createLoginView.php

--- view/createLoginView.php
+++ view/createLoginView.php
@@ -1,4 +1,3 @@
-
 <?php require('menuView.php'); ?>
 <?php require('loginView.php'); ?>
 
@@ -6,38 +5,43 @@
 
 <?php ob_start(); ?>
 
-        <section id="formulaireInscription">
+<section id="formulaireInscription">
+
+    <h1>Inscription</h1>
+
+    <div>
+        <form action="index.php?action=createAccount" method="post">
+            <p><em>Les champs obligatoires sont indiqués avec *</em></p>
             
-            <h1>Inscription</h1>
+            <p>
+                <label for="pseudo">Pseudo*</label><br />
+                <input type="text" id="pseudo" name="pseudo" required /><span id="alertPseudo"></span>
+            </p>
             
-            <div>
-                
-                <form action="index.php?action=createAccount" method="post">
-                    <p><em>Les champs obligatoires sont indiqués avec *</em></p>
-                    <p>
-                        <label for="pseudo">Pseudo*</label><br />
-                        <input type="text" id="pseudo" name="pseudo" required /><span id="alertPseudo"></span>
-                    </p>
-                    <p>
-                        <label for="email">Adresse Email*</label><br />
-                        <input type="email" id="email" name="email" required /><span id="alertEmail"></span>
-                    </p>
-                    <p>
-                        <label for="password">Mot de passe*</label><br />
-                        <input type="password" id="password" name="password" required /><span id="alertPassword"></span>
-                    </p>
-                    <p>
-                        <label for="verifPassword">Vérification du mot de passe*</label><br />
-                        <input type="Password" id="verifPassword" name="verifPassword" minlength="8" required /><span id="alertVerifPassword"></span>
-                    </p>
-                    <p>
-                        <input type="submit" value="S'inscrire" />
-                    </p>
-                </form>
-                <p><a href="index.php">Retour à la liste des billets</a></p>
-            </div>
+            <p>
+                <label for="email">Adresse Email*</label><br />
+                <input type="email" id="email" name="email" required /><span id="alertEmail"></span>
+            </p>
             
-        </section>
+            <p>
+                <label for="password">Mot de passe*</label><br />
+                <input type="password" id="password" name="password" required /><span id="alertPassword"></span>
+            </p>
+            
+            <p>
+                <label for="verifPassword">Vérification du mot de passe*</label><br />
+                <input type="Password" id="verifPassword" name="verifPassword" minlength="8" required /><span id="alertVerifPassword"></span>
+            </p>
+            
+            <p>
+                <input type="submit" value="S'inscrire" />
+            </p>
+        </form>
+        
+        <p><a href="index.php">Retour à la liste des billets</a></p>
+    </div>
+
+</section>
 
 <?php $content = ob_get_clean(); ?>
 
