fdbcb7eced0c96f7060e0dc87382b443a57c4f5d
PascalTed <monemail@email.com>
2018-10-31 08:58:37 +0100

    Déplacé balise em et ajouté balise br dans view/admListPostsView.php

--- view/admListPostsView.php
+++ view/admListPostsView.php
@@ -1,13 +1,13 @@
 <?php require('menuView.php'); ?>
 <?php require('loginView.php'); ?>
 
-<?php $titleHeader = 'Administration du Blog de Jean Forteroche' ?>
+<?php $titleHeader = 'Administration du Blog' ?>
 
 <?php ob_start(); ?>
 
 <section class="section-one">
     
-    <h1>Billets du blog à modifier ou supprimer</h1>
+    <h1>Billets du blog à modifier ou à supprimer</h1>
     
     <div class="link-adm">
         <p><a href="index.php?action=admCreatePost">Ajouter des billets</a><a href="index.php?action=admReportComment">Commentaires signalés</a></p>
@@ -33,8 +33,8 @@
     <!-- Affichage du billet -->
     <div class="news">
         <h2>
+            <em>le <?= $data['creation_date_fr']; ?></em><br />
             <?= $data['title']; ?>
-            <em>le <?= $data['creation_date_fr']; ?></em>
         </h2>
         
         <div class="news-contenu">
