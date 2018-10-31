9d2af0af6599de0fa24f899f8120f2c92b4482c5
PascalTed <monemail@email.com>
2018-10-31 01:53:56 +0100

    Nettoyé code dans view/admReportCommentView.php

--- view/admReportCommentView.php
+++ view/admReportCommentView.php
@@ -1,19 +1,17 @@
 <?php require('menuView.php'); ?>
 <?php require('loginView.php'); ?>
 
-<?php $titleHeader = 'Administration du Blog de Jean Forteroche' ?>
-
+<?php $titleHeader = 'Administration du Blog' ?>
 
 <?php ob_start(); ?>
-
 
 <section class="section-one">
 
     <h1>Commentaires signalés</h1>
     
     <div class="link-adm">
-    <p><a href="index.php?action=admCreatePost">Ajouter des billets</a><a href="index.php?action=admListPosts">Billets créés</a></p>
-</div>
+        <p><a href="index.php?action=admCreatePost">Ajouter des billets</a><a href="index.php?action=admListPosts">Billets créés</a></p>
+    </div>
     
     <?php
     $countReportComment = $comments->rowcount();
@@ -23,15 +21,17 @@
             <p>
                 <strong>Titre du billet : </strong><?= $comment['title'] ?>
             </p>
+            
             <p>
                 <strong>Commentaire signalé : </strong><br />
                 "<?= htmlspecialchars($comment['comment']); ?>"
             </p>
+            
             <p>
                 <a href="index.php?action=admValidComment&amp;idComment=<?= $comment['id'] ?>">Valider le commentaire</a><a href="index.php?action=admRemoveComment&amp;idComment=<?= $comment['id'] ?>">Supprimer le commentaire</a>    
             </p>
-
         </div>
+    
     <?php
     }
     if ($countReportComment == 0) {
@@ -39,13 +39,12 @@
         <div>
             <p>Il n'y a pas de commentaires signalés</p>
         </div>
+    
     <?php
     }
     $comments->closeCursor();
     ?>
- 
-
-    
+     
 </section>
 
 <?php $content = ob_get_clean(); ?>
