9fcbbc8a7eb4d088b30074cd11f1c32871e0493d
PascalTed <monemail@email.com>
2018-11-02 09:54:36 +0100

    Ajouté id extrait dans view/listPostsView.php

--- view/listPostsView.php
+++ view/listPostsView.php
@@ -35,7 +35,7 @@
 
         <div class="news-contenu">
             <?= $postExtract; ?>
-            <a href="index.php?action=post&amp;idPost=<?= $data['id']; ?>"> <em><strong>... lire la suite</strong></em></a>
+            <a id="extrait" href="index.php?action=post&amp;idPost=<?= $data['id']; ?>"> <em><strong>... lire la suite</strong></em></a>
         </div>
     </div>
     
